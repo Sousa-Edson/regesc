5e4df31949178538d15fa3204ab65fbd484f65a8
Edson Sousa <edson3711.es@gmail.com>
2022-12-18 08:37:42 -0300

    criando entidade

---
+++ src/main/java/com/edson/regesc/orm/Professor.java
@@ -0,0 +1,17 @@
+package com.edson.regesc.orm;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Professores")
+public class Professor {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+   private Long id; 
+   private String nome;
+   private String prontuario;
+}

a72a10acedb9472dc745c1417f4af59b8785c2ca
Edson Sousa <edson3711.es@gmail.com>
2022-12-18 13:01:34 -0300

    Create DisciplinaRepository.java

---
+++ src/main/java/com/edson/regesc/repository/DisciplinaRepository.java
@@ -0,0 +1,9 @@
+package com.edson.regesc.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.edson.regesc.orm.Disciplina;
+
+public interface DisciplinaRepository extends CrudRepository<Disciplina, Long> {
+
+}

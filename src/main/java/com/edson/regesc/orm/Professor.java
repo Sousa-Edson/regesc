a63c8cbd48bae4647996692b38c8dc34f36a7bb5
Edson Sousa <edson3711.es@gmail.com>
2022-12-18 08:44:53 -0300

    update

--- src/main/java/com/edson/regesc/orm/Professor.java
+++ src/main/java/com/edson/regesc/orm/Professor.java
@@ -1,17 +1,19 @@
 package com.edson.regesc.orm;
 
+import org.springframework.data.annotation.Id;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Professores")
 public class Professor {
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
-   private Long id; 
-   private String nome;
-   private String prontuario;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String prontuario;
+
 }

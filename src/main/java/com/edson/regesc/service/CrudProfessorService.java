0a648cf0fc4ccebdc48a311bb994dcb05a22ccca
Edson Sousa <edson3711.es@gmail.com>
2022-12-18 12:01:13 -0300

    Update CrudProfessorService.java

--- src/main/java/com/edson/regesc/service/CrudProfessorService.java
+++ src/main/java/com/edson/regesc/service/CrudProfessorService.java
@@ -23,6 +23,8 @@ public class CrudProfessorService {
             System.out.println("0 - Voltar ao menu anterior");
             System.out.println("1 - cadastrar novo professor");
             System.out.println("2 - atualizar um professor");
+            System.out.println("3 - visualizar todos os professores");
+            System.out.println("4- deletar um professor");
             int opcao = scanner.nextInt();
             switch (opcao) {
                 case 1:
@@ -31,7 +33,12 @@ public class CrudProfessorService {
                 case 2:
                     atualizar(scanner);
                     break;
-
+                case 3:
+                    visualizar();
+                    break;
+                case 4:
+                    deletar(scanner);
+                    break;
                 default:
                     isTrue = false;
                     break;
@@ -51,7 +58,7 @@ public class CrudProfessorService {
     }
 
     private void atualizar(Scanner scanner) {
-        System.out.println("\nDigite o ID do professor a ser atualizado:\n");
+        System.out.print("\nDigite o ID do professor a ser atualizado:");
         Long id = scanner.nextLong();
         Optional<Professor> optional = this.professorRepository.findById(id);
         if (optional.isPresent()) {
@@ -70,5 +77,25 @@ public class CrudProfessorService {
         } else {
             System.out.println("O ID do professor informado: " + id + " é inválido\n");
         }
+    }
+
+    private void visualizar() {
+        System.out.println();
+        Iterable<Professor> professores = this.professorRepository.findAll();
+        for (Professor professor : professores) {
+            System.out.println(professor.toString());
+        }
+        // professores.forEach(professor ->{
+        // System.out.println(professor.toString());
+        // });
+        // professores.forEach(System.out::println);
+        System.out.println();
+    }
+
+    private void deletar(Scanner scanner) {
+        System.out.print("\nDigite o ID do professor a ser deletado:");
+        Long id = scanner.nextLong();
+        this.professorRepository.deleteById(id);
+        System.out.println("\nProfessor deletado\n");
     }
 }

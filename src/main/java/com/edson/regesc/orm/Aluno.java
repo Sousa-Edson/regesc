9dc06228b8458cee74245ddcfc795fabde391983
Edson Sousa <edson3711.es@gmail.com>
2022-12-20 07:53:55 -0300

    Update Aluno.java

--- src/main/java/com/edson/regesc/orm/Aluno.java
+++ src/main/java/com/edson/regesc/orm/Aluno.java
@@ -1,5 +1,25 @@
 package com.edson.regesc.orm;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "alunos")
 public class Aluno {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    private Integer idade;
+
+    @ManyToMany(mappedBy = "alunos")
+    List<Disciplina> disciplinas;
 }
